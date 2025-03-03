488e1ddd334fd8df96f3e0b7213f966f6c83edaa
edu-delahoz <eduardodelahoz@unisinu.edu.co>
2025-03-02 19:05:15 -0500

    Agregado modelo de Establishment con campos genéricos

---
+++ src/models/Establishment.js
@@ -0,0 +1,65 @@
+const {DataTypes} = require("sequelize");
+const sequelize = require("../config/database");
+
+
+const Establishment = sequelize.define("Establishment", {
+
+    id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4, 
+        primaryKey: true,
+        allowNull: false
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            isEmail: true
+          }
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    },
+    website: {
+        type: DataTypes.STRING,
+        allowNull: true
+
+    },
+    latitude: {
+        type: DataTypes.DECIMAL(10, 8),
+        allowNull: false
+
+    },
+    longitude: {
+        type: DataTypes.DECIMAL(11, 8),
+        allowNull: false
+
+    },
+    imageUrl: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+
+},{
+    timestamps: true
+});
+
+module.exports = Establishment;
